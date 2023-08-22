5220f60a76dcb6dcd0f5021f2f72e9469e628ce4
Rafael Crespim <rafa.cechinato@gmail.com>
2023-08-22 12:03:04 -0300

    Test made by Rafael

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import moment, { Moment } from 'moment';
+import axios from "axios";
 
 import logo from './abc-glofox-logo.png';
 import './App.css';
@@ -37,6 +38,15 @@ function isTrainer(x: Staff): x is Trainer {
   return x.type === 'trainer';
 }
 
+async function getTrainers() {
+  try {
+    const response = await axios.get(STAFF_ENDPOINT)
+    return response.data.filter((item:Staff) => isTrainer(item))
+  } catch (error) {
+    return ["error"] 
+  }
+}
+
 const config = {
   name: {
     id: 'name',
@@ -71,26 +81,60 @@ const config = {
   }
 }
 
-function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+function onSubmit(e: any,data: object) {
+  e.preventDefault()
   // POST to ${APPOINTMENTS_ENDPOINT}
   // Should match AppointmentAPIRequestBody type
+  console.log("data",data)
 }
 
 function App() {
+  const [trainer,setTrainer] = useState([])
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    if(trainer.length === 0){
+      callTrainer()
+    }
+  },[]);
+
+  async function callTrainer(){
+    const response = await getTrainers();
+    setTrainer(response)
+  }
+
+  function handleChange(e: any){
+    setData({...data, [e.target.name]: e.target.value});
+  }
+
   return (
     <div className="App">
       <header className="app__header">
         <img src={logo} className="ApP--logo" alt="logo" />
       </header>
       <main className="appmain">
-        <form onSubmit={onSubmit}>
-          <input id='name' name='name' />
-          <label className="field">
-            Email:
-            <input id='email' name='email' />
+        <form onSubmit={(e) => onSubmit(e,data)}>
+          <label className="label">
+            Name:
+            <input required id='name' name='name' onChange={(e) => handleChange(e)} />
           </label>
-          <input  id='dateTime' type="datetime-local" />
-          <input  id='trainer' name='trainer' />
+          <label className="label email-input">
+            Email:
+            <input required id='email' name='email' onChange={(e) => handleChange(e)} />
+          </label>
+          <label className="label ">
+            Date:
+            <input required className='date-input' name="datetime" onChange={(e) => handleChange(e)} id='dateTime' type="datetime-local" />
+          </label>
+          <label className="label-trainer">
+            Trainer:
+            <select required id='trainer' name="trainer" onChange={(e) => handleChange(e)}>
+              {trainer.map( (item: any, index) => (
+                <option key={index} value={`${item.id}`}>{item.name}</option>
+              ))} 
+            </select>
+          </label>
+          
           <input type="submit" />
         </form>
       </main>
